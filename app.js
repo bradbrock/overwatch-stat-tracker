957be492f503d390b59a8145c6dfe840fa8c5cf5
Brad Brock <brad.brock1@gmail.com>
2018-01-20 15:55:39 -0600

    Created a MongoDB and connection to app.

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
 
 const app = express();
 
 require('dotenv').config({ path: 'variables.env' });
 
-// app.get('/', function (req, res) {
-//   res.send('BAR');
+app.get('/', function (req, res) {
+  res.send('FOO');
+});
 
-// });
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
 
 module.exports = app;
