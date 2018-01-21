9b4e1160cb0c25729995338539b431013b111563
Brad Brock <brad.brock1@gmail.com>
2018-01-20 20:35:12 -0600

    Added Pug, created layout template.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 // const router = express.Router();
 const session = require('express-session');
 const mongoose = require('mongoose');
-// const path = require('path');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
 
-require('dotenv').config({ path: 'variables.env' });
+// view engine setup
+app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
+app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
 
 app.get('/', function (req, res) {
   res.send('bar');
