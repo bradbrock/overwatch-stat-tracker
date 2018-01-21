2859b4272248fb95989282a560c7359ff667e5e1
Brad Brock <brad.brock1@gmail.com>
2018-01-21 14:56:34 -0600

    Linked stylesheet to layout template

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
 app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
 
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+app.use(express.static(path.join(__dirname, 'public')));
+
 // router
 app.use('/', routes);
 
